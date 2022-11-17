233e67a9d50cb9a83154fb8bc21b0ad26347c00a
abernatskiy <abernatskiy@toha.li>
2022-11-18 05:15:39 +0900

    Cosmetics
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -3,8 +3,8 @@ import {EvmBatchProcessor} from '@subsquid/evm-processor'
 import * as lendingPoolAbi from './abi/aave-lending-pool-v2'
 import {LiquidationEvent} from './model'
 
-// The so-called AAVE V2 (https://etherscan.io/address/0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9) - starts at 11362579
-// Tis' a proxy, Implementation is at 0xc6845a5c768bf8d7681249f8927877efda425baf
+// The so-called AAVE V2 (0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9) - starts at 11362579
+// Tis' a proxy, implementation is at 0xc6845a5c768bf8d7681249f8927877efda425baf
 
 const processor = new EvmBatchProcessor()
 	.setBlockRange({from: 11362579})
@@ -13,7 +13,9 @@ const processor = new EvmBatchProcessor()
 		archive: 'https://eth.archive.subsquid.io',
 	})
 	.addLog('0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9', {
-		filter: [[lendingPoolAbi.events['LiquidationCall(address,address,address,uint256,uint256,address,bool)'].topic]],
+		filter: [[lendingPoolAbi.events[
+			'LiquidationCall(address,address,address,uint256,uint256,address,bool)'
+		].topic]],
 		data: {
 			evmLog: {
 				id: true,
@@ -65,4 +67,3 @@ processor.run(new TypeormDatabase(), async (ctx) => {
 
 	await ctx.store.save(liquidations)
 });
-

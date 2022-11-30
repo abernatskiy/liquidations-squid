fa5716a4952457bbbec6950aa8f25d10c18272f6
abernatskiy <abernatskiy@toha.li>
2022-12-01 03:47:19 +0900

    Reformatting for compactness + comments added
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -6,24 +6,19 @@ import {LiquidationEvent} from './model'
 // The so-called AAVE V2 (0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9) - starts at 11362579
 // Tis' a proxy, implementation is at 0xc6845a5c768bf8d7681249f8927877efda425baf
 
+// Grab events with a signature LiquidationCall(...)
+// emitted by contract 0x7d2.. (aka AAVE V2 Lending Pool),
+// keep id, topics and data for the event
+// and a hash of the parent transaction
+
 const processor = new EvmBatchProcessor()
 	.setBlockRange({from: 11362579})
-	.setDataSource({
-		archive: 'https://eth-stage1.archive.subsquid.io',
-	})
+	.setDataSource({archive: 'https://eth-stage1.archive.subsquid.io'})
 	.addLog('0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9', {
-		filter: [[lendingPoolAbi.events[
-			'LiquidationCall(address,address,address,uint256,uint256,address,bool)'
-		].topic]],
+		filter: [[lendingPoolAbi.events['LiquidationCall(address,address,address,uint256,uint256,address,bool)'].topic]],
 		data: {
-			evmLog: {
-				id: true,
-				topics: true,
-				data: true
-			},
-			transaction: {
-				hash: true
-			}
+			evmLog: { id: true, topics: true, data: true },
+			transaction: { hash: true }
 		} as const
 	})
 
@@ -34,16 +29,9 @@ processor.run(new TypeormDatabase(), async (ctx) => {
 		for (let i of c.items) {
 			if (i.kind==='evmLog') {
 				const {
-					collateralAsset,
-					debtAsset,
-					user,
-					debtToCover,
-					liquidatedCollateralAmount,
-					liquidator,
-					receiveAToken
-				} = lendingPoolAbi.events[
-					'LiquidationCall(address,address,address,uint256,uint256,address,bool)'
-				].decode(i.evmLog)
+					collateralAsset, debtAsset, user, debtToCover,
+					liquidatedCollateralAmount, liquidator, receiveAToken
+				} = lendingPoolAbi.events['LiquidationCall(address,address,address,uint256,uint256,address,bool)'].decode(i.evmLog)
 				const block = c.header.height
 				const hash = i.transaction.hash
 				const eventId = i.evmLog.id

e93269c858b796f82df5ef54882dcc0b6cb3c839
abernatskiy <abernatskiy@toha.li>
2022-12-08 06:37:29 +0900

    An attempt to migrate to the new store interface
    	modified:   .gitignore
    	new file:   csv/.keep
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -1,12 +1,7 @@
-import {TypeormDatabase} from '@subsquid/typeorm-store'
 import {EvmBatchProcessor} from '@subsquid/evm-processor'
+import {CsvDatabase, Table, types} from '@subsquid/csv-store'
 import * as lendingPoolAbi from './abi/aave-lending-pool-v2'
 import * as fs from 'fs'
-
-const csvPath = 'aave-liquidations.csv'
-if (!fs.existsSync(csvPath)) {
-	fs.writeFileSync(csvPath, 'id,collateralAsset,debtAsset,user,debtToCover,liquidatedCollateralAmount,liquidator,receiveAToken,block,timestamp,transactionHash\n');
-}
 
 // The so-called AAVE V2 (0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9) - starts at 11362579
 // Tis' a proxy, implementation is at 0xc6845a5c768bf8d7681249f8927877efda425baf
@@ -18,13 +13,30 @@ const processor = new EvmBatchProcessor()
 		data: { evmLog: { id: true, topics: true, data: true }, transaction: { hash: true } } as const
 	})
 
-processor.run(new TypeormDatabase(), async (ctx) => {
-	let csvWriter = fs.createWriteStream(csvPath, { flags: 'a' })
+// Translation of schema.graphql to the csv-store speak
+const LiquidationEventsTable = new Table('liquidationEvents', {
+	id: types.string,
+	collateralAsset: types.string,
+	debtAsset: types.string,
+	user: types.string,
+	debtToCover: types.bigint,
+	liquidatedCollateralAmount: types.bigint,
+	liquidator: types.string,
+	receiveAToken: types.boolean,
+	block: types.bigint,
+	timestamp: types.bigint,
+	hash: types.string
+})
+
+const db = new CsvDatabase([LiquidationEventsTable], {dest: `./csv/`, chunkSize: 10})
+
+processor.run(db, async (ctx) => {
+	let liquidationEventsBuffer: any[] = []
 
 	for (let c of ctx.blocks) {
 		for (let i of c.items) {
 			if (i.kind==='evmLog') {
-				let eventId = i.evmLog.id
+				let id = i.evmLog.id
 				let { collateralAsset, debtAsset, user, debtToCover,
 					liquidatedCollateralAmount, liquidator, receiveAToken
 				} = lendingPoolAbi.events.LiquidationCall.decode(i.evmLog)
@@ -32,11 +44,16 @@ processor.run(new TypeormDatabase(), async (ctx) => {
 				let timestamp = c.header.timestamp
 				let hash = i.transaction.hash
 
-				csvWriter.write(`${eventId},${collateralAsset},${debtAsset},${user},${debtToCover.toBigInt().toString()},`+
-					`${liquidatedCollateralAmount.toBigInt().toString()},${liquidator},${receiveAToken},${block},${timestamp},${hash}\n`)
+				liquidationEventsBuffer.push({
+					id, collateralAsset, debtAsset, user, debtToCover,
+					liquidatedCollateralAmount, liquidator, receiveAToken,
+					block, timestamp, hash
+				})
 			}
 		}
 	}
 
-	csvWriter.close()
+	for (let le of liquidationEventsBuffer) {
+		ctx.store.write(LiquidationEventsTable, le)
+	}
 });

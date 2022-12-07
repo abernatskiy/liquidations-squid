a45a5419abc9d468fb2e0b3a81ab89b2d238fede
abernatskiy <abernatskiy@toha.li>
2022-12-07 16:43:13 +0900

    ORM autogenerated code updated
    	modified:   src/model/generated/liquidationEvent.model.ts

--- src/model/generated/liquidationEvent.model.ts
+++ src/model/generated/liquidationEvent.model.ts
@@ -3,42 +3,47 @@ import * as marshal from "./marshal"
 
 @Entity_()
 export class LiquidationEvent {
-  constructor(props?: Partial<LiquidationEvent>) {
-    Object.assign(this, props)
-  }
+    constructor(props?: Partial<LiquidationEvent>) {
+        Object.assign(this, props)
+    }
 
-  @PrimaryColumn_()
-  id!: string
+    @PrimaryColumn_()
+    id!: string
 
-  @Index_()
-  @Column_("text", {nullable: false})
-  collateralAsset!: string
+    @Index_()
+    @Column_("text", {nullable: false})
+    collateralAsset!: string
 
-  @Index_()
-  @Column_("text", {nullable: false})
-  debtAsset!: string
+    @Index_()
+    @Column_("text", {nullable: false})
+    debtAsset!: string
 
-  @Index_()
-  @Column_("text", {nullable: false})
-  user!: string
+    @Index_()
+    @Column_("text", {nullable: false})
+    user!: string
 
-  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
-  debtToCover!: bigint
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    debtToCover!: bigint
 
-  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
-  liquidatedCollateralAmount!: bigint
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    liquidatedCollateralAmount!: bigint
 
-  @Index_()
-  @Column_("text", {nullable: false})
-  liquidator!: string
+    @Index_()
+    @Column_("text", {nullable: false})
+    liquidator!: string
 
-  @Column_("bool", {nullable: false})
-  receiveAToken!: boolean
+    @Column_("bool", {nullable: false})
+    receiveAToken!: boolean
 
-  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
-  block!: bigint
+    @Index_()
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    block!: bigint
 
-  @Index_()
-  @Column_("text", {nullable: false})
-  hash!: string
+    @Index_()
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    timestamp!: bigint
+
+    @Index_()
+    @Column_("text", {nullable: false})
+    hash!: string
 }

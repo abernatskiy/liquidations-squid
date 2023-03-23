29e81cc208a9166dfad611e95b951d576de69f7c
abernatskiy <abernatskiy@toha.li>
2023-03-24 05:13:00 +0900

    Using archive registry in processor
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -2,6 +2,7 @@ import {TypeormDatabase} from '@subsquid/typeorm-store'
 import {EvmBatchProcessor} from '@subsquid/evm-processor'
 import * as lendingPoolAbi from './abi/aave-lending-pool-v2'
 import {LiquidationEvent} from './model'
+import {lookupArchive} from '@subsquid/archive-registry'
 
 // The so-called AAVE V2 (0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9) - starts at 11362579
 // Tis' a proxy, implementation is at 0xc6845a5c768bf8d7681249f8927877efda425baf
@@ -13,7 +14,7 @@ import {LiquidationEvent} from './model'
 
 const processor = new EvmBatchProcessor()
 	.setBlockRange({from: 11362579})
-	.setDataSource({archive: 'https://eth.archive.subsquid.io'})
+	.setDataSource({archive: lookupArchive('eth-mainnet')})
 	.addLog('0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9', {
 		filter: [[lendingPoolAbi.events.LiquidationCall.topic]],
 		data: {
